0e1e6ce0c21b87fce4bcb3fc9dc5023fb560b047
adrian.morari <adrian.morari@LT00007871>
2022-09-09 11:03:34 +0300

    Add project files.

---
+++ SuggestionAppLibrary/Models/BasicSuggestionModel.cs
@@ -0,0 +1,20 @@
+﻿namespace SuggestionAppLibrary.Models;
+public class BasicSuggestionModel
+{
+   [BsonId]
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string Title { get; set; }
+
+   public BasicSuggestionModel()
+   {
+
+   }
+
+   public BasicSuggestionModel(SuggestionModel suggestion)
+   {
+      Id = suggestion.Id;
+      Title = suggestion.Suggestion;
+   }
+
+}
